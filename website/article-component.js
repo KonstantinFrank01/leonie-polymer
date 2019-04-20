8efa10160ec08d63a39d9323fcb7fc78a24a0b7c
davidandraschko <d.andraschko@students.htl-leonding.ac.at>
2019-04-20 12:45:48 +0200

    Bilder und Textüberschrift eingefügt

--- website/article-component.js
+++ website/article-component.js
@@ -6,10 +6,10 @@ class ArticleComponent extends LitElement {
       <link rel="stylesheet" href="article.css" />
 
       <div id="box">
-        <div id="article1"></div>
-        <div id="article2"></div>
-        <div id="article3"></div>
-        <div id="article4"></div>
+        <div id="article1"><img src = "images/code.jpeg"><div id ="white1"><span class="title">Blick in technologische Zukunft</span></div></img></div>
+        <div id="article2"><img src = "images/girl.jpeg"><div id ="white2"><span class="title">Neues aus der Zukunft</span></div></img></div>
+        <div id="article3"><img src = "images/meeting.jpeg"><div id ="white3"><span class="title">Schlossmuseum wirft einen Blick in die Welt der Zukunft</span></div></img></div>
+        <div id="article4"><img src = "images/pupils.jpeg"><div id ="white4"><span class="title">TechVisionen - Neues aus der Zukunft</span></div></img></div>
       </div>
     `;
   }
